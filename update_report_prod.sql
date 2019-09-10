03d01879658001525ff56d82a293ac3ecf79505b
Muhammad Usama <lifeh2o@gmail.com>
2019-09-10 14:46:48 +0500

    Create update_report_prod.sql

---
+++ update_report_prod.sql
@@ -0,0 +1,6 @@
+REPLACE VIEW DEV_QA_PROD.MidRangeProducts AS 
+	SELECT DEV_QA_PROD.Company.Name as CompanyName,
+	       DEV_QA_PROD.Product.Name as ProductName,
+	       DEV_QA_PROD.Product.Price as Price
+	FROM DEV_QA_PROD.Company, DEV_QA_PROD.Product
+	WHERE DEV_QA_PROD.Product.CompanyID = DEV_QA_PROD.Company.ID AND Price > 200 AND Price < 1000;
